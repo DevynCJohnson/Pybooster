70340230d73d5ff9375b410c579e70ccf12551ad
DevynCJohnson <DevynCJohnson@Gmail.com>
2018-09-11 08:21:02 -0500

    Added aliases to docker_rc.sh

--- accessory/shell_ext_modules/docker_rc.sh
+++ accessory/shell_ext_modules/docker_rc.sh
@@ -15,25 +15,38 @@
 if [ -x "$(command -v docker)" ]; then
 
 
-alias docker_conrm='docker container rm'
-alias docker_constop='docker container stop'
-alias docker_imgls='docker images'
-alias docker_rm_sock='sudo rm -r -f /var/run/docker.sock'
-alias docker_srvmk='docker service create'
-alias docker_srvps='docker service ps'
-alias docker_srvrm='docker service rm'
-alias docker_volinspect='docker volume inspect'
-alias docker_volls='docker volume ls'
-alias docker_volmk='docker volume create'
-alias docker_volprune='docker volume prune'
-alias docker_volrm='docker volume rm'
+alias dock_cl='docker container ls -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}"'  # List containers (compressed list)
+alias docker_clean_all='docker system prune --all'  # Remove all unused images not just dangling ones
+alias docker_clean_con='docker container prune'  # Remove all stopped containers
+alias docker_clean_img='docker image prune --all'  # Remove all unused images, not just dangling ones
+alias docker_conls='docker ps -a'  # List containers
+alias docker_conrm='docker container rm'  # Remove container
+alias docker_constop='docker container stop'  # Stop container
+alias docker_exe='docker run -i -t'  # Run Docker image like an executable
+alias docker_imgls='docker images'  # List images
+alias dock_plugls='docker plugin ls'  # List plugins
+alias dock_rm_sock='sudo rm -r -f /var/run/docker.sock'  # Remove Docker's socket
+alias dock_srvmk='docker service create'  # Create service
+alias dock_srvps='docker service ps'  # View services
+alias dock_srvrm='docker service rm'  # Remove service
+alias dock_volinspect='docker volume inspect'  # Inspect volume
+alias dock_volls='docker volume ls'  # List volumes
+alias dock_volmk='docker volume create'  # Create volumes
+alias dock_volprune='docker volume prune'  # Prune unused volumes
+alias dock_volrm='docker volume rm'  # Remove volumes
+
+
+if [ -x "$(command -v docker-compose)" ]; then
+    alias dockdown='docker-compose down'
+    alias dockup='docker-compose up -d'
+fi
 
 
 if [ -x "$(command -v service)" ]; then
-    alias docker_stat='service docker status'
-    alias docker_restart='sudo service docker restart'
+    alias dock_stat='service docker status'
+    alias dock_restart='sudo service docker restart'
 elif [ -x "$(command -v systemctl)" ]; then
-    alias docker_stat='systemctl status docker.service'
+    alias dock_stat='systemctl status docker.service'
 fi
 
 
