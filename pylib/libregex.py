6081e8c0953377aef26e4311e9421f6b6fd33ddb
DevynCJohnson <DevynCJohnson@Gmail.com>
2018-10-06 12:20:40 -0500

    Added regex patterns in many Unicode blocks in libregex.py

--- pylib/libregex.py
+++ pylib/libregex.py
@@ -64,6 +64,71 @@ __all__: list = [
     r'ARABIC',
     r'SYRIAC',
     r'ARABIC_SUPPLEMENT',
+    r'THAANA',
+    r'NKO',
+    r'SAMARITAN',
+    r'MANDAIC',
+    r'SYRIAC_SUPPLEMENT',
+    r'ARABIC_EXTENDED_A',
+    r'DEVANAGARI',
+    r'BENGALI',
+    r'GURMUKHI',
+    r'GUJARATI',
+    r'ORIYA',
+    r'TAMIL',
+    r'TELUGU',
+    r'KANNADA',
+    r'MALAYALAM',
+    r'SINHALA',
+    r'THAI',
+    r'LAO',
+    r'TIBETAN',
+    r'MYANMAR',
+    r'GEORGIAN',
+    r'HANGUL_JAMO',
+    r'ETHIOPIC',
+    r'ETHIOPIC_SUPPLEMENT',
+    r'CHEROKEE',
+    r'UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS',
+    r'OGHAM',
+    r'RUNIC',
+    r'TAGALOG',
+    r'HANUNOO',
+    r'BUHID',
+    r'TAGBANWA',
+    r'KHMER',
+    r'MONGOLIAN',
+    r'UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS_EXTENDED',
+    r'LIMBU',
+    r'TAI_LE',
+    r'NEW_TAI_LUE',
+    r'KHMER_SYMBOLS',
+    r'BUGINESE',
+    r'TAI_THAM',
+    r'COMBINING_DIACRITICAL_MARKS_EXTENDED',
+    r'BALINESE',
+    r'SUNDANESE',
+    r'BATAK',
+    r'LEPCHA',
+    r'OL_CHIKI',
+    r'CYRILLIC_EXTENDED_C',
+    r'GEORGIAN_EXTENDED',
+    r'SUNDANESE_SUPPLEMENT',
+    r'VEDIC_EXTENSIONS',
+    r'PHONETIC_EXTENSIONS',
+    r'PHONETIC_EXTENSIONS_SUPPLEMENT',
+    r'COMBINING_DIACRITICAL_MARKS_SUPPLEMENT',
+    r'LATIN_EXTENDED_ADDITIONAL',
+    r'GREEK_EXTENDED',
+    r'GENERAL_PUNCTUATION',
+    r'SUPERSCRIPTS_AND_SUBSCRIPTS',
+    r'CURRENCY_SYMBOLS',
+    r'COMBINING_DIACRITICAL_MARKS_FOR_SYMBOLS',
+    r'LETTERLIKE_SYMBOLS',
+    r'NUMBER_FORMS',
+    r'ARROWS',
+    r'MATHEMATICAL_OPERATORS',
+    r'MISCELLANEOUS_TECHNICAL',
     # XML SOURCE CODE PATTERNS #
     r'CHARREF',
     r'INCOMPLETE_REF',
@@ -126,6 +191,71 @@ HEBREW = rgxcompile(r'[\u0590-\u05FF]+')
 ARABIC = rgxcompile(r'[\u0600-\u06FF]+')
 SYRIAC = rgxcompile(r'[\u0700-\u074F]+')
 ARABIC_SUPPLEMENT = rgxcompile(r'[\u0750-\u077F]+')
+THAANA = rgxcompile(r'[\u0780-\u07BF]+')
+NKO = rgxcompile(r'[\u07C0-\u07FF]+')
+SAMARITAN = rgxcompile(r'[\u0800-\u083F]+')
+MANDAIC = rgxcompile(r'[\u0840-\u085F]+')
+SYRIAC_SUPPLEMENT = rgxcompile(r'[\u0860-\u086F]+')
+ARABIC_EXTENDED_A = rgxcompile(r'[\u08A0-\u08FF]+')
+DEVANAGARI = rgxcompile(r'[\u0900-\u097F]+')
+BENGALI = rgxcompile(r'[\u0980-\u09FF]+')
+GURMUKHI = rgxcompile(r'[\u0A00-\u0A7F]+')
+GUJARATI = rgxcompile(r'[\u0A80-\u0AFF]+')
+ORIYA = rgxcompile(r'[\u0B00-\u0B7F]+')
+TAMIL = rgxcompile(r'[\u0B80-\u0BFF]+')
+TELUGU = rgxcompile(r'[\u0C00-\u0C7F]+')
+KANNADA = rgxcompile(r'[\u0C80-\u0CFF]+')
+MALAYALAM = rgxcompile(r'[\u0D00-\u0D7F]+')
+SINHALA = rgxcompile(r'[\u0D80-\u0DFF]+')
+THAI = rgxcompile(r'[\u0E00-\u0E7F]+')
+LAO = rgxcompile(r'[\u0E80-\u0EFF]+')
+TIBETAN = rgxcompile(r'[\u0F00-\u0FFF]+')
+MYANMAR = rgxcompile(r'[\u1000-\u109F]+')
+GEORGIAN = rgxcompile(r'[\u10A0-\u10FF]+')
+HANGUL_JAMO = rgxcompile(r'[\u1100-\u11FF]+')
+ETHIOPIC = rgxcompile(r'[\u1200-\u137F]+')
+ETHIOPIC_SUPPLEMENT = rgxcompile(r'[\u1380-\u139F]+')
+CHEROKEE = rgxcompile(r'[\u13A0-\u13FF]+')
+UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS = rgxcompile(r'[\u1400-\u167F]+')
+OGHAM = rgxcompile(r'[\u1680-\u169F]+')
+RUNIC = rgxcompile(r'[\u16A0-\u16FF]+')
+TAGALOG = rgxcompile(r'[\u1700-\u171F]+')
+HANUNOO = rgxcompile(r'[\u1720-\u173F]+')
+BUHID = rgxcompile(r'[\u1740-\u175F]+')
+TAGBANWA = rgxcompile(r'[\u1760-\u177F]+')
+KHMER = rgxcompile(r'[\u1780-\u17FF]+')
+MONGOLIAN = rgxcompile(r'[\u1800-\u18AF]+')
+UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS_EXTENDED = rgxcompile(r'[\u18B0-\u18FF]+')
+LIMBU = rgxcompile(r'[\u1900-\u194F]+')
+TAI_LE = rgxcompile(r'[\u1950-\u197F]+')
+NEW_TAI_LUE = rgxcompile(r'[\u1980-\u19DF]+')
+KHMER_SYMBOLS = rgxcompile(r'[\u19E0-\u19FF]+')
+BUGINESE = rgxcompile(r'[\u1A00-\u1A1F]+')
+TAI_THAM = rgxcompile(r'[\u1A20-\u1AAF]+')
+COMBINING_DIACRITICAL_MARKS_EXTENDED = rgxcompile(r'[\u1AB0-\u1AFF]+')
+BALINESE = rgxcompile(r'[\u1B00-\u1B7F]+')
+SUNDANESE = rgxcompile(r'[\u1B80-\u1BBF]+')
+BATAK = rgxcompile(r'[\u1BC0-\u1BFF]+')
+LEPCHA = rgxcompile(r'[\u1C00-\u1C4F]+')
+OL_CHIKI = rgxcompile(r'[\u1C50-\u1C7F]+')
+CYRILLIC_EXTENDED_C = rgxcompile(r'[\u1C80-\u1C8F]+')
+GEORGIAN_EXTENDED = rgxcompile(r'[\u1C90-\u1CBF]+')
+SUNDANESE_SUPPLEMENT = rgxcompile(r'[\u1CC0-\u1CCF]+')
+VEDIC_EXTENSIONS = rgxcompile(r'[\u1CD0-\u1CFF]+')
+PHONETIC_EXTENSIONS = rgxcompile(r'[\u1D00-\u1D7F]+')
+PHONETIC_EXTENSIONS_SUPPLEMENT = rgxcompile(r'[\u1D80-\u1DBF]+')
+COMBINING_DIACRITICAL_MARKS_SUPPLEMENT = rgxcompile(r'[\u1DC0-\u1DFF]+')
+LATIN_EXTENDED_ADDITIONAL = rgxcompile(r'[\u1E00-\u1EFF]+')
+GREEK_EXTENDED = rgxcompile(r'[\u1F00-\u1FFF]+')
+GENERAL_PUNCTUATION = rgxcompile(r'[\u2000-\u206F]+')
+SUPERSCRIPTS_AND_SUBSCRIPTS = rgxcompile(r'[\u2070-\u209F]+')
+CURRENCY_SYMBOLS = rgxcompile(r'[\u20A0-\u20CF]+')
+COMBINING_DIACRITICAL_MARKS_FOR_SYMBOLS = rgxcompile(r'[\u20D0-\u20FF]+')
+LETTERLIKE_SYMBOLS = rgxcompile(r'[\u2100-\u214F]+')
+NUMBER_FORMS = rgxcompile(r'[\u2150-\u218F]+')
+ARROWS = rgxcompile(r'[\u2190-\u21FF]+')
+MATHEMATICAL_OPERATORS = rgxcompile(r'[\u2200-\u22FF]+')
+MISCELLANEOUS_TECHNICAL = rgxcompile(r'[\u2300-\u23FF]+')
 
 
 # XML SOURCE CODE PATTERNS #
