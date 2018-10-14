96dc9ebafafe4aaef61a0e8b7b5d78ecd1986ac3
DevynCJohnson <DevynCJohnson@Gmail.com>
2018-10-14 17:32:42 -0500

    Improved documentation, optimized code, added code, & added doctests to cryptography.py

--- pylib/cryptography.py
+++ pylib/cryptography.py
@@ -29,89 +29,121 @@ along with this software.
 """
 
 
+from binascii import crc32, crc_hqx
 from itertools import cycle
 
 
 __all__: list = [
+    # ENCRYPTION/DECRYPTION #
     r'encrypt_xor',
     r'decrypt_xor',
     r'encrypt_xor_bytes_key_str',
     r'decrypt_xor_str_key_bytes',
+    r'encrypt_xor_str_key_str',
     r'decrypt_xor_str_key_str',
     r'encrypt_xor_str_key_bytes',
     r'decrypt_xor_bytes_key_bytes',
-    r'decrypt_xor_bytes_key_str'
+    r'decrypt_xor_bytes_key_str',
+    # CHECKSUMS #
+    r'get_checksums'
 ]
 
 
-def encrypt_xor(_data: bytes) -> bytearray:
-    """XOR Encryption
+# ENCRYPTION/DECRYPTION #
 
-    Return the encrypted data as a bytearray
-    Decrypt with decrypt_xor()
+
+def encrypt_xor(_data: str) -> bytearray:
+    r"""Return the xor-encrypted data (of the given string) as a bytearray; Decrypt with decrypt_xor()
+
+    >>> encrypt_xor(r'This is a test.')
+    bytearray(b'\xfc\xc0\xc1\xdb\x88\xc1\xdb\x88\xc9\x88\xdc\xcd\xdb\xdc\x86')
     """
-    return bytearray([_bytes ^ 0xA8 for _bytes in bytearray(_data)])
+    return bytearray([_bytes ^ 0xA8 for _bytes in bytearray(bytes(_data, encoding=r'utf-8'))])
 
 
 def decrypt_xor(_data: bytearray) -> str:
-    """XOR Decryption
+    r"""Return the xor-decrypted data (of the given byte-array) as a string; Encrypt with encrypt_xor()
 
-    Return the decrypted data as a str
-    Encrypt with encrypt_xor()
+    >>> decrypt_xor(bytearray(b'\xfc\xc0\xc1\xdb\x88\xc1\xdb\x88\xc9\x88\xdc\xcd\xdb\xdc\x86'))
+    'This is a test.'
     """
     return str(bytearray([_bytes ^ 0xA8 for _bytes in bytearray(_data)]), r'utf-8')
 
 
 def encrypt_xor_bytes_key_str(_data: bytes, _key: str) -> str:
-    """XOR encryption with custom key
+    r"""XOR-Encrypt the bytes using the given key and return the data as a string; Decrypt with decrypt_xor_str_key_bytes()
 
-    Encrypt the bytes and return the data as a str
-    Decrypt with decrypt_xor_str_key_bytes()
+    >>> encrypt_xor_bytes_key_str(br'This is a test.', r'Coffee')
+    '\x17\x07\x0f\x15E\x0c0O\x07F\x11\x000\x1bH'
     """
     return r''.join(chr(a ^ ord(b)) for (a, b) in zip(_data, cycle(_key)))
 
 
 def decrypt_xor_str_key_bytes(_data: str, _key: str) -> bytes:
-    """XOR decryption with custom key
+    r"""XOR-Decrypt the string using the given key and return the data as bytes; Encrypt with encrypt_xor_bytes_key_str()
 
-    Decrypt the str and return the data as bytes
-    Encrypt with encrypt_xor_bytes_key_str()
+    >>> decrypt_xor_str_key_bytes('\x17\x07\x0f\x15E\x0c0O\x07F\x11\x000\x1bH', r'Coffee')
+    b'This is a test.'
     """
     return r''.join(chr(a ^ ord(b)) for (a, b) in zip(_data.encode(r'utf-8'), cycle(_key))).encode(r'utf-8')
 
 
-def decrypt_xor_str_key_str(_data: str, _key: str) -> str:
-    """XOR decryption with custom key
+def encrypt_xor_str_key_str(_data: str, _key: str) -> str:
+    r"""XOR-Encrypt the string using the given key and return the data as a string; Decrypt with decrypt_xor_str_key_bytes()
 
-    Decrypt the str and return the data as a str
-    Encrypt with encrypt_xor_bytes_key_str()
+    >>> encrypt_xor_str_key_str(r'This is a test.', r'Coffee')
+    '\x17\x07\x0f\x15E\x0c0O\x07F\x11\x000\x1bH'
+    """
+    return r''.join(chr(a ^ ord(b)) for (a, b) in zip(bytes(_data, encoding=r'utf-8'), cycle(_key)))
+
+
+def decrypt_xor_str_key_str(_data: str, _key: str) -> str:
+    r"""XOR-Decrypt the string using the given key and return the data as a string; Encrypt with encrypt_xor_str_key_str()
+
+    >>> decrypt_xor_str_key_str('\x17\x07\x0f\x15E\x0c0O\x07F\x11\x000\x1bH', r'Coffee')
+    'This is a test.'
     """
     return r''.join(chr(a ^ ord(b)) for (a, b) in zip(_data.encode(r'utf-8'), cycle(_key)))
 
 
 def encrypt_xor_str_key_bytes(_data: str, _key: str) -> bytes:
-    """XOR encryption with custom key
+    r"""Return the xor-encrypted string data as bytes using the given key; Decrypt with decrypt_xor_bytes_key_bytes() or decrypt_xor_bytes_key_str()
 
-    Return the encrypted str data as bytes
-    Encrypt the str and return the data as bytes
-    Decrypt with decrypt_xor_bytes_key_bytes() or decrypt_xor_bytes_key_str()
+    >>> encrypt_xor_str_key_bytes(r'This is a test.', r'Coffee')
+    b'\x17\x07\x0f\x15E\x0c0O\x07F\x11\x000\x1bH'
     """
     return r''.join(chr(a ^ ord(b)) for (a, b) in zip(_data.encode(r'utf-8'), cycle(_key))).encode(r'utf-8')
 
 
 def decrypt_xor_bytes_key_bytes(_data: bytes, _key: str) -> bytes:
-    """XOR decryption with custom key
+    r"""XOR-Decrypt the bytes using the given key and return the data as bytes; Encrypt with encrypt_xor_str_key_bytes()
 
-    Decrypt the bytes and return the data as bytes
-    Encrypt with encrypt_xor_str_key_bytes()
+    >>> decrypt_xor_bytes_key_bytes(b'\x17\x07\x0f\x15E\x0c0O\x07F\x11\x000\x1bH', r'Coffee')
+    b'This is a test.'
     """
     return r''.join(chr(a ^ ord(b)) for (a, b) in zip(_data, cycle(_key))).encode(r'utf-8')
 
 
 def decrypt_xor_bytes_key_str(_data: bytes, _key: str) -> str:
-    """XOR decryption with custom key
+    r"""XOR-Decrypt the bytes using the given key and return the data as a string; Encrypt with encrypt_xor_str_key_bytes()
 
-    Decrypt the bytes and return the data as a str
-    Encrypt with encrypt_xor_str_key_bytes()
+    >>> decrypt_xor_bytes_key_str(b'\x17\x07\x0f\x15E\x0c0O\x07F\x11\x000\x1bH', r'Coffee')
+    'This is a test.'
     """
     return r''.join(chr(a ^ ord(b)) for (a, b) in zip(_data, cycle(_key)))
+
+
+# CHECKSUMS #
+
+
+def get_checksums(_data: object) -> dict:
+    """Generate a dictionary containing various checksum values for the given data
+
+    >>> get_checksums(br'This is a test.')
+    {'CRC32': 94035228, 'CRC16': 50568}
+    """
+    _out: dict = {r'CRC32': 0, r'CRC16': 0}
+    _bytedata: bytes = bytes(str(_data), encoding=r'utf-8')
+    _out[r'CRC32'] = crc32(_bytedata)
+    _out[r'CRC16'] = crc_hqx(_bytedata, 0)
+    return _out
