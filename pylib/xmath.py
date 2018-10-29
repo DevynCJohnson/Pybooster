15bfc12c7e36b64d95b575b3c1dd22619d950215
DevynCJohnson <DevynCJohnson@Gmail.com>
2018-10-28 19:04:03 -0500

    Added more functions & applied optimizations to xmath.py

--- pylib/xmath.py
+++ pylib/xmath.py
@@ -34,7 +34,7 @@ along with this software.
 
 from math import acos, asin, atan, cos, erf, log, sin, sqrt, tan
 from random import choice
-from typing import Union
+from typing import Iterable, Tuple, Union
 
 
 __all__: list = [
@@ -107,6 +107,9 @@ __all__: list = [
     r'factors',
     r'phi',
     # STATISTICS #
+    r'avgoffset',
+    r'is_improvement',
+    r'is_improvement_meta',
     r'showpercent',
     # PROBABILITY #
     r'dice',
@@ -575,7 +578,7 @@ def factors(_num: int) -> set:
     >>> factors(196)
     {1, 2, 98, 196, 4, 7, 14, 49, 28}
     """
-    return set(x for tup in ([i, _num // i] for i in range(1, int(_num ** 0.5) + 1) if _num % i == 0) for x in tup)
+    return {x for tup in ([i, _num // i] for i in range(1, int(_num ** 0.5) + 1) if _num % i == 0) for x in tup}
 
 
 def phi(_num: float) -> float:
@@ -594,6 +597,48 @@ def phi(_num: float) -> float:
 # STATISTICS #
 
 
+def avgoffset(results_list: Iterable[Union[float, int]], target_list: Iterable[Union[float, int]]) -> float:
+    """Compute the average offset of the list of results versus the target list
+
+    >>> avgoffset([3.04606776, 6.04589842, 8.0331831, 9.03312666, 12.03295732, 12.99509362, 14.02024201, 15.99492428], [3.0, 6.0, 8.0, 9.0, 12.0, 13.0, 14.0, 16.0])
+    0.02768217125000022
+    >>> avgoffset([3.41, 6.03, 7.89, 8.36, 10.03, 12.22, 13.15, 13.3], [3.0, 6.0, 8.0, 9.0, 12.0, 13.0, 14.0, 16.0])
+    0.93625
+    """
+    _offset: float = 0.0
+    for x, y in zip(results_list, target_list):
+        _offset += abs(x - y)  # type: ignore
+    return _offset / len(results_list)  # type: ignore
+
+
+def is_improvement(new_list: Iterable[Union[float, int]], best_list: Iterable[Union[float, int]], target_list: Iterable[Union[float, int]]) -> bool:
+    """Return true if the new results are closer to the target than the previous results
+
+    >>> is_improvement([3.04606776, 6.04589842, 8.0331831, 9.03312666, 12.03295732, 12.99509362, 14.02024201, 15.99492428], [3.41, 6.03, 7.89, 8.36, 10.03, 12.22, 13.15, 13.3], [3.0, 6.0, 8.0, 9.0, 12.0, 13.0, 14.0, 16.0])
+    True
+    """
+    _newlist_mean_offset: float = 0.0
+    for x, y in zip(new_list, target_list):
+        _newlist_mean_offset += abs(x - y)  # type: ignore
+    _newlist_mean_offset /= len(new_list)  # type: ignore
+    _bestlist_mean_offset: float = 0.0
+    for x, y in zip(best_list, target_list):
+        _bestlist_mean_offset += abs(x - y)  # type: ignore
+    _bestlist_mean_offset /= len(best_list)  # type: ignore
+    return True if _bestlist_mean_offset > _newlist_mean_offset else False
+
+
+def is_improvement_meta(new_list: Iterable, best_list: Iterable, target_list: Iterable) -> Tuple[bool, float, float]:
+    """Return true as the first item in the tuple if the new results are closer to the target than the previous results; Returns a tuple containing the boolean value of the comparison, the lowest offset, and the largest offset
+
+    >>> is_improvement_meta([3.04606776, 6.04589842, 8.0331831, 9.03312666, 12.03295732, 12.99509362, 14.02024201, 15.99492428], [3.41, 6.03, 7.89, 8.36, 10.03, 12.22, 13.15, 13.3], [3.0, 6.0, 8.0, 9.0, 12.0, 13.0, 14.0, 16.0])
+    (True, 0.02768217125000022, 0.93625)
+    """
+    _newlist_mean_offset: float = avgoffset(new_list, target_list)
+    _bestlist_mean_offset: float = avgoffset(best_list, target_list)
+    return (True, _newlist_mean_offset, _bestlist_mean_offset) if _bestlist_mean_offset >= _newlist_mean_offset else (False, _bestlist_mean_offset, _newlist_mean_offset)
+
+
 def showpercent(_portion: Union[float, int], _total: Union[float, int] = 100.0) -> str:
     """Convert a portion and total to a percentage
 
@@ -603,6 +648,8 @@ def showpercent(_portion: Union[float, int], _total: Union[float, int] = 100.0) 
     '37.00%'
     >>> showpercent(37, 1000)
     '3.70%'
+    >>> showpercent(73, 10)
+    '730.00%'
     """
     return r'{:.2%}'.format(float(_portion) / float(_total))
 
@@ -642,12 +689,12 @@ def clamp(_num: float, smallest: float = -1.0, largest: float = 1.0) -> float:
     73.0
     >>> clamp(3.14159, 17.0, 73.0)
     17.0
+    >>> clamp(73.0, 17.0, 73.0)
+    73.0
+    >>> clamp(3.14159, 3.14159, 73.0)
+    3.14159
     """
-    if _num > largest:
-        return largest
-    elif _num < smallest:
-        return smallest
-    return _num
+    return largest if _num >= largest else smallest if _num <= smallest else _num
 
 
 def int2many(_int: int) -> str:
