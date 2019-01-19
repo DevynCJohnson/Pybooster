d925cccd9a8a4b4be0d3a0736899da19fb6e7549
DevynCJohnson <DevynCJohnson@Gmail.com>
2019-01-19 11:33:38 -0600

    Miscellaneous tweaks & changes
    Signed-off-by: DevynCJohnson <DevynCJohnson@Gmail.com>

--- makefile_constants.mk
+++ makefile_constants.mk
@@ -1250,22 +1250,24 @@ COMPILED_HEADERS::=$(CLANG) $(ARCH) $(STD) -x c-header
 
 genchecksums :
 	-@printf '\x1b[1;4;33m%s\x1b[0m\n\n' '=== Creating Checksums ==='
-	$(RM) ./checksums.md5 ./checksums.sha512 ./checksums.sha1
-	find . -mount -type f -not \( -name 'checksums.md5' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 md5sum --tag > "${FOLDER}checksums.md5"
-	find . -mount -type f -not \( -name 'checksums.sha512' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 sha512sum --tag > "${FOLDER}checksums.sha512"
-	find . -mount -type f -not \( -name 'checksums.sha1' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 sha1sum --tag > "${FOLDER}checksums.sha1"
+	$(RM) ./checksums.b2b512 ./checksums.rmd160 ./checksums.sha1 ./checksums.sha512 ./checksums.whirlpool || true
+	find . -mount -type f -not \( -name 'checksums.sha512' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 openssl dgst -sha512 > "${FOLDER}checksums.sha512"
+	find . -mount -type f -not \( -name 'checksums.whirlpool' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 openssl dgst -whirlpool > "${FOLDER}checksums.whirlpool"
+	find . -mount -type f -not \( -name 'checksums.rmd160' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 openssl dgst -ripemd160 > "${FOLDER}checksums.rmd160"
+	find . -mount -type f -not \( -name 'checksums.b2b512' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 openssl blake2b512 > "${FOLDER}checksums.b2b512"
+	find . -mount -type f -not \( -name 'checksums.sha1' -o -path "./bin/*" -o -path "./debugging/*" -o -path "./testing/*" -o -path "./.git/*" \) -print0 | xargs -0 openssl dgst -sha1 > "${FOLDER}checksums.sha1"
 
 valchecksums :
 	-@printf '\x1b[1;4;33m%s\x1b[0m\n\n' '=== Validating Checksums ==='
-	md5sum --check --quiet --strict ./checksums.md5
 	sha512sum --check --quiet --strict ./checksums.sha512
+	b2sum --check --quiet --strict ./checksums.b2b512
 	sha1sum --check --quiet --strict ./checksums.sha1
 
 
 # GIT COMMANDS #
 
 
-.PHONY : cleangit commit commithash commithashes gitattr gitchanges gitignore gitlastchanges gitlinechanges gitlscommits gitlsconfigs gitlsfiles gitstats lscontrib lstags newchanges news previewcleangit pushtags setkey signoff status tag treehashes uncommit xcommit xtag
+.PHONY : cleangit commit commithash commithashes gitattr gitchanges gitignore gitlastchanges gitlinechanges gitlscommits gitlsconfigs gitlsfiles gitstats gitundoadd lscontrib lstags newchanges news previewcleangit pushtags setkey signoff status tag treehashes uncommit xcommit xtag
 
 
 commit :
@@ -1317,6 +1319,9 @@ gitlsfiles :
 gitstats :
 	-@gitstats ./ $(TESTINGDIR)/git/
 
+gitundoadd :
+	-@git reset
+
 lscontrib :
 	@git log --format='%aN <%aE>' | awk '{ arr[$0]++ } END { for (i in arr) { print arr[i], i; } }' | sort -n -r | cut -d ' ' -f 2-
 
@@ -1326,9 +1331,6 @@ lstags :
 newchanges :
 	@git diff
 
-news :
-	@([ -f ./ChangeLog ] && $(COPY) ./ChangeLog ./NEWS) || true
-
 previewcleangit : cleanall fixperm
 	-@git reflog expire --dry-run --all --expire=now --stale-fix
 	git rm --dry-run --cached -r --ignore-unmatch *
@@ -1336,7 +1338,8 @@ previewcleangit : cleanall fixperm
 	git add --dry-run --all --refresh
 
 pushtags :
-	@git push origin --tags
+	@printf '\x1b[1;4;33m%s\x1b[0m\n\n' '=== Pushing Tags ==='
+	git push origin --tags
 
 setkey :
 	@git config --global user.signingkey $(key); git config --global commit.gpgsign true; git config --global gpg.program "$(shell which gpg)"
@@ -1361,3 +1364,17 @@ xcommit :
 
 xtag :
 	@git tag -s -a 'v$(__VERSION__)' -m 'Stable Release (v$(__VERSION__))'
+
+
+# MISCELLANEOUS PROJECT COMMANDS #
+
+
+.PHONY : changelogmd news
+
+changelogmd : ./tools/mkchangelogmd.sh
+	@printf '\x1b[1;4;33m%s\x1b[0m\n\n' '=== Creating Markdown ChangeLog ==='
+	$(RM) ./ChangeLog.md
+	./tools/mkchangelogmd.sh
+
+news :
+	@([ -f ./ChangeLog ] && $(COPY) ./ChangeLog ./NEWS) || true
---
+++ tools/mkchangelogmd.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# -*- coding: utf-8-unix; Mode: Shell; indent-tabs-mode: nil; tab-width: 4 -*-
+# vim: set fileencoding=utf-8 filetype=shell syn=sh.doxygen fileformat=unix tabstop=4 expandtab :
+# kate: encoding utf-8; bom off; syntax shell; indent-mode normal; eol unix; replace-tabs on; indent-width 4; tab-width 4; remove-trailing-space on; line-numbers on;
+#' @brief Convert ChangeLog to ChnageLog.md
+#' @file mkchangelogmd.sh
+#' @version 2019.01.18
+#' @author Devyn Collier Johnson <DevynCJohnson@Gmail.com>
+#' @copyright Public Domain (CC0) - https://creativecommons.org/publicdomain/zero/1.0/
+
+
+# MAIN #
+
+
+if [ ! -r './ChangeLog' ] || [ ! -f './ChangeLog' ]; then
+    printf 'ERROR: ./ChangeLog was not found or is not readable!\n' >&2
+    exit 1
+else
+    awk NF < ./ChangeLog | sed -E -e 's|^[ \t]*\* ([^:]+):  (.+)$|- **\1:**  \2|; s|^([0-9]{4})\-([0-9]{2})\-([0-9]{2}) (.+) <(.+)>$|##\1-\2-\3 \4 <[\5](mailto:\5)>|;' > ./ChangeLog.md
+fi
