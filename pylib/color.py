e28356c25bb35ec5df36b0819ee19c47c8fc8e63
DevynCJohnson <DevynCJohnson@Gmail.com>
2019-06-10 18:47:53 -0500

    Replaced `utf-8-unix` with `utf-8` in the Emacs comment of the PyLib Python code
    Signed-off-by: DevynCJohnson <DevynCJohnson@Gmail.com>

--- pylib/color.py
+++ pylib/color.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8-unix; Mode: Python; indent-tabs-mode: nil; tab-width: 4 -*-
+# -*- coding: utf-8; Mode: Python; indent-tabs-mode: nil; tab-width: 4 -*-
 # vim: set fileencoding=utf-8 filetype=python syntax=python.doxygen fileformat=unix tabstop=4 expandtab :
 # kate: encoding utf-8; bom off; syntax python; indent-mode python; eol unix; replace-tabs off; indent-width 4; tab-width 4; remove-trailing-space on; line-numbers on;
 """@brief Color manipulation
