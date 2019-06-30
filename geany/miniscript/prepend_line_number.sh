1402edb001495727d633dba59f8b9867e882b2ab
DevynCJohnson <DevynCJohnson@Gmail.com>
2019-06-29 19:14:17 -0500

    Added `prepend_line_number.sh` to the Miniscript directory in Geany
    Signed-off-by: DevynCJohnson <DevynCJohnson@Gmail.com>

---
+++ geany/miniscript/prepend_line_number.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# -*- coding: utf-8-unix; Mode: Shell; indent-tabs-mode: nil; tab-width: 4 -*-
+# vim: set fileencoding=utf-8 filetype=shell syn=sh.doxygen fileformat=unix tabstop=4 expandtab :
+# kate: encoding utf-8; bom off; syntax shell; indent-mode normal; eol unix; replace-tabs on; indent-width 4; tab-width 4; remove-trailing-space on; line-numbers on;
+#' @brief Prepend the number of the line to each of the file's lines
+#' @file prepend_line_number.sh
+#' @version 2019.06.23
+#' @author Devyn Collier Johnson <DevynCJohnson@Gmail.com>
+
+
+nl -w1
