093a81d2769bbfe6c3f1cad8e4fccd9f45d91892
DevynCJohnson <DevynCJohnson@Gmail.com>
2019-10-11 18:51:44 -0500

    Improved the declaration of `_start` in start_x86_64.s
    Signed-off-by: DevynCJohnson <DevynCJohnson@Gmail.com>

--- include/start_x86_64.s
+++ include/start_x86_64.s
@@ -2,7 +2,11 @@
 /* vim: set fileencoding=utf-8 filetype=asm syntax=asm asmsyntax=gas fileformat=unix tabstop=4 : */
 /* kate: encoding utf-8; bom off; syntax gas; indent-mode normal; eol unix; replace-tabs on; indent-width 4; tab-width 4; remove-trailing-space on; */
 
-.globl _start
+
+.section .text
+	.globl _start
+	.type _start, @function
+
 
 _start:
 	movq %rsp, %rbp  /* Setup a new stack frame */
@@ -15,5 +19,6 @@ _start:
 	movl $1, %eax
 	xorl %ebx, %ebx
 	int $0x80
+
 
 .size _start, . - _start
