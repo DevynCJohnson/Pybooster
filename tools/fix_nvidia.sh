5ac465550d81ab661bb23bc0f4934934c49dc1cb
DevynCJohnson <DevynCJohnson@Gmail.com>
2019-12-28 21:22:03 -0600

    Made the /`dev/null` code consistent in fix_nvidia.sh
    Signed-off-by: DevynCJohnson <DevynCJohnson@Gmail.com>

--- tools/fix_nvidia.sh
+++ tools/fix_nvidia.sh
@@ -4,7 +4,7 @@
 # kate: encoding utf-8; bom off; syntax shell; indent-mode normal; eol unix; replace-tabs on; indent-width 4; tab-width 4; remove-trailing-space on;
 #' @brief Change various system settings to fix some issues with the proprietary Nvidia driver
 #' @file fix_nvidia.sh
-#' @version 2019.03.28
+#' @version 2019.12.28
 #' @author Devyn Collier Johnson <DevynCJohnson@Gmail.com>
 #' @copyright Public Domain (CC0) - https://creativecommons.org/publicdomain/zero/1.0/
 
@@ -15,8 +15,8 @@
 if [ ! "$(id -u)" -eq 0 ]; then
     exec sudo -- "$0"
 else
-    { getent group nvidia-persistenced 2>&1; } > /dev/null || groupadd -g 143 nvidia-persistenced
-    { getent passwd nvidia-persistenced 2>&1; } > /dev/null || useradd -c 'NVIDIA Persistence Daemon' -u 143 -g nvidia-persistenced -d '/' -s /sbin/nologin nvidia-persistenced
+    { getent group nvidia-persistenced 2>&1 > /dev/null; } || groupadd -g 143 nvidia-persistenced
+    { getent passwd nvidia-persistenced 2>&1 > /dev/null; } || useradd -c 'NVIDIA Persistence Daemon' -u 143 -g nvidia-persistenced -d '/' -s /sbin/nologin nvidia-persistenced
     getent group root | grep -F -q -v 'nvidia-persistenced' && usermod -a -G root nvidia-persistenced
     getent group nvidia-persistenced | grep -F -q -v 'root' && usermod -a -G nvidia-persistenced root
 fi
